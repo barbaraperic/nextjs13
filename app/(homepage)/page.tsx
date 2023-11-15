24099297ca12a59bc05b38fdfc81cbfb39f83913
Barbara Peric <b.peric10@gmail.com>
2023-11-15 15:41:44 +0000

    banner for the homepage

--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import dayjs from "dayjs";
+import PortugueseFlagComponent from "../components/icons/portugueseFlag";
 
 export default function Homepage() {
 	const dailyTasks = [
@@ -12,24 +13,27 @@ export default function Homepage() {
 
 	return (
 		<main className="relative h-4/6 flex justify-center items-center">
-			<div className="">
+			<div className=" relative">
 				<h2 className="text-center">
 					Track your learning progress and <br></br> create habits to master{" "}
 					<br></br> Portuguese
 				</h2>
-				<span className="uppercase rotate-3 absolute font-script text-xl w-28 left-0 top-[20%]">
-					create your own dictionary
-				</span>
-				<span className="uppercase rotate-3 absolute font-script text-xl w-28 left-[10%] top-[75%]">
-					track daily, weekly and monthly habits
-				</span>
-				<span className="uppercase rotate-3 absolute font-script text-xl w-28 right-0 top-[10%]">
-					save and review articles
-				</span>
-				<span className="uppercase rotate-3 absolute font-script text-xl w-28 right-[10%] top-[65%]">
-					get word of the day in your inbox
-				</span>
+				<div className="w-40 absolute right-28">
+					<PortugueseFlagComponent />
+				</div>
 			</div>
+			<span className="uppercase rotate-3 absolute font-script text-xl w-28 left-0 top-[20%]">
+				create your own dictionary
+			</span>
+			<span className="uppercase rotate-3 absolute font-script text-xl w-28 left-[10%] top-[75%]">
+				track daily, weekly and monthly habits
+			</span>
+			<span className="uppercase rotate-3 absolute font-script text-xl w-28 right-0 top-[10%]">
+				save and review articles
+			</span>
+			<span className="uppercase rotate-3 absolute font-script text-xl w-28 right-[10%] top-[65%]">
+				get word of the day in your inbox
+			</span>
 		</main>
 	);
 }
---
+++ app/components/icons/portugueseFlag.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+
+function PortugueseFlagComponent(props: any) {
+	return (
+		<>
+			<svg
+				height="100%"
+				viewBox="0 0 16.122967 1.6643041"
+				version="1.1"
+				id="svg1"
+				xmlns="http://www.w3.org/2000/svg"
+				{...props}>
+				<g id="layer1" transform="translate(-135.691,-114.44435)">
+					<g id="g23">
+						<path
+							style={{
+								fill: "#046a38",
+								fillOpacity: 1,
+								stroke: "#046a38",
+								strokeWidth: 0.3,
+								strokeLinecap: "round",
+								strokeLinejoin: "bevel",
+								strokeDasharray: "none",
+								strokeOpacity: 1,
+								paintOrder: "stroke fill markers",
+							}}
+							d="m 135.84101,115.27459 c 0.0713,-0.0143 0.14126,-0.0382 0.21379,-0.0428 0.98341,-0.0615 2.01222,0 2.99303,0 1.80413,0 3.21002,-0.0224 5.0454,0.0855 0.99961,0.0588 1.99538,0.1708 2.99304,0.25654 2.83653,0.24376 1.8746,0.15998 4.40404,0.38482"
+							id="path23"
+						/>
+						<path
+							style={{
+								fill: "#da291c",
+								fillOpacity: 1,
+								stroke: "#da291c",
+								strokeWidth: 0.3,
+								strokeLinecap: "round",
+								strokeLinejoin: "bevel",
+								strokeDasharray: "none",
+								strokeOpacity: 1,
+								paintOrder: "stroke fill markers",
+							}}
+							d="m 136.01467,114.66462 c 0.0713,-0.0143 0.14126,-0.0382 0.21379,-0.0428 0.98341,-0.0615 2.01222,0 2.99303,0 1.80413,0 3.21002,-0.0224 5.0454,0.0855 0.99961,0.0588 1.99538,0.1708 2.99304,0.25654 2.83653,0.24376 1.8746,0.15998 4.40404,0.38482"
+							id="path23-8"
+						/>
+					</g>
+				</g>
+			</svg>
+		</>
+	);
+}
+
+export default PortugueseFlagComponent;
