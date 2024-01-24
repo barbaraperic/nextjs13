2616c64434589887513e2584cd89008ed8afd52f
Barbara Peric <b.peric10@gmail.com>
2024-01-24 10:16:17 +0000

    react flow inputs

---
+++ components/CustomNode.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import React, { memo } from 'react'
+import { Handle, Position } from 'reactflow'
+
+const CustomNode = ({ data }) => {
+  return (
+    <div className="px-4 py-2 text-center shadow-md rounded-md bg-white border-2 border-stone-400">
+      <div className="flex">
+        <div className="ml-2">
+          <div className="text-md font-bold text-gray-500">{data.name}</div>
+          <div className="text-gray-500">{data.function}</div>
+        </div>
+      </div>
+
+      <Handle
+        type="target"
+        position={Position.Top}
+        className="w-16 !bg-teal-500"
+      />
+      <Handle
+        type="source"
+        position={Position.Bottom}
+        className="w-16 !bg-teal-500"
+      />
+    </div>
+  )
+}
+
+export default memo(CustomNode)
--- components/Flow.tsx
+++ components/Flow.tsx
@@ -3,36 +3,52 @@
 import React, { useEffect, useState } from 'react'
 import ReactFlow, { useNodesState, useEdgesState } from 'reactflow'
 import 'reactflow/dist/style.css'
+import CustomNode from './CustomNode'
+
+const nodeTypes = {
+  custom: CustomNode,
+}
 
 const initialNodes = [
-  { id: '1', data: { label: '-' }, position: { x: 100, y: 100 } },
-  { id: '2', data: { label: 'Node 2' }, position: { x: 100, y: 200 } },
+  {
+    id: '1',
+    type: 'custom',
+    data: { name: 'Jane Doe', function: 'noun' },
+    position: { x: 100, y: 100 },
+  },
+  {
+    id: '2',
+    type: 'custom',
+    data: { name: 'Jane Doe', function: 'noun' },
+    position: { x: 100, y: 200 },
+  },
 ]
 
 const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }]
 const defaultViewport = { x: 0, y: 0, zoom: 1.5 }
 
-function onNodeClick(event, node) {
-  console.log('click node', node)
-}
-
-const UpdateNode = () => {
+const InteractiveFlow = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
+  const [nodeId, setNodeId] = useState(null)
+  const [style, setStyle] = useState({ backgroundColor: '#14ff50' })
 
   const [nodeName, setNodeName] = useState('')
+  const [nodeFunction, setNodeFunction] = useState('')
   const [nodeBg, setNodeBg] = useState('')
+
+  function onNodeClick(event, node) {
+    setNodeId(node.id)
+    setStyle({ backgroundColor: '#ff1493' })
+  }
 
   useEffect(() => {
     setNodes((nds) =>
       nds.map((node) => {
-        console.log(node)
-        if (node.id === '2') {
-          // it's important that you create a new object here
-          // in order to notify react flow about the change
+        if (node.id === nodeId) {
           node.data = {
             ...node.data,
-            label: nodeName,
+            name: nodeName,
           }
         }
 
@@ -44,9 +60,22 @@ const UpdateNode = () => {
   useEffect(() => {
     setNodes((nds) =>
       nds.map((node) => {
-        if (node.id === '2') {
-          // it's important that you create a new object here
-          // in order to notify react flow about the change
+        if (node.id === nodeId) {
+          node.data = {
+            ...node.data,
+            function: nodeFunction,
+          }
+        }
+
+        return node
+      })
+    )
+  }, [nodeFunction, setNodes])
+
+  useEffect(() => {
+    setNodes((nds) =>
+      nds.map((node) => {
+        if (node.id === nodeId) {
           node.style = { ...node.style, backgroundColor: nodeBg }
         }
 
@@ -67,19 +96,31 @@ const UpdateNode = () => {
       maxZoom={4}
       attributionPosition="bottom-left"
       onNodeClick={onNodeClick}
+      nodeTypes={nodeTypes}
     >
-      <div className="absolute right-3 top-3 z-10 text-base text-black">
-        <label className="block">label:</label>
+      <div className="absolute right-3 top-3 z-10 text-base text-black space-y-2">
+        <label className="block text-white">Word:</label>
         <input
+          className="p-1 rounded"
           value={nodeName}
           onChange={(evt) => setNodeName(evt.target.value)}
         />
+        <label className="block text-white">Function:</label>
+        <input
+          className="p-1 rounded"
+          value={nodeFunction}
+          onChange={(evt) => setNodeFunction(evt.target.value)}
+        />
 
-        <label className="mt-3">background:</label>
-        <input value={nodeBg} onChange={(evt) => setNodeBg(evt.target.value)} />
+        <label className="mt-3 block text-white">Background:</label>
+        <input
+          className="p-1 rounded"
+          value={nodeBg}
+          onChange={(evt) => setNodeBg(evt.target.value)}
+        />
       </div>
     </ReactFlow>
   )
 }
 
-export default UpdateNode
+export default InteractiveFlow
