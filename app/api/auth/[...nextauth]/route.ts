51bbac7aa9344a9bb017d33678d438dff121f89c
Barbara Peric <b.peric10@gmail.com>
2024-02-06 15:10:15 +0100

    check none on provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -11,6 +11,7 @@ export const authOptions: NextAuthOptions = {
       name: 'GitHub',
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+      checks: ['none'],
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET as string,
