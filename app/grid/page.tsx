2e83324e3636af9ec68ee01cec653ad7161cf70c
Barbara Peric <b.peric10@gmail.com>
2024-03-07 14:42:44 +0000

    grid WIP

---
+++ app/grid/page.tsx
@@ -0,0 +1,53 @@
+import styles from './gridPage.module.scss'
+
+export default function GridPage() {
+  return (
+    <main>
+      <div className={styles.wrapper}>
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['expand']}`}>
+          <h1>Start your note taking journey</h1>
+        </div>
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['a']}`}></div>
+        <div className={`${styles['box']} ${styles['a']}`}></div>
+        <div className={`${styles['box']} ${styles['a']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>{' '}
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>{' '}
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>{' '}
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>{' '}
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>{' '}
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>{' '}
+        <div className={`${styles['box']} ${styles['']}`}></div>{' '}
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>{' '}
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>{' '}
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>{' '}
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['']}`}></div>
+        <div className={`${styles['box']} ${styles['expand2']}`}>
+          Try Tartaruga for free here
+        </div>
+      </div>
+    </main>
+  )
+}
